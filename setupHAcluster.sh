d2b42ce09f0d70db5c92455890304f02b8b82f92
sai411 <66815779+sai411@users.noreply.github.com>
2023-08-15 11:52:21 +0530

    Create setupHAcluster.sh

---
+++ setupHAcluster.sh
@@ -0,0 +1,22 @@
+apt-get update
+apt-get install -y docker.io
+apt-get update 
+apt-get install -y apt-transport-https
+apt-get install curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+# only in master
+# Update the n/w cidr
+kubeadm init --pod-network-cidr=10.0.0.0/16
+#To start using your cluster, you need to run (as a regular user):
+#  mkdir -p $HOME/.kube
+#  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#You should now deploy a pod network to the cluster.
+#Run "kubectl apply -f [podnetwork].yaml" with one of the options listed at:
+#  https://kubernetes.io/docs/concepts/cluster-administration/addons/
