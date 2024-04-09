a8a479ba85c9f05ff1a62675506405994056d975
sai411 <66815779+sai411@users.noreply.github.com>
2024-04-09 11:56:29 +0530

    Update setupHAcluster.sh

--- setupHAcluster.sh
+++ setupHAcluster.sh
@@ -1,6 +1,8 @@
 Lagecy installations using package managers have been deprecated use this steps without package manager from here 
 https://v1-28.docs.kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 script is availble at install-utilities_k8s.sh
+
+RBAC:  https://aungzanbaw.medium.com/a-step-by-step-guide-to-creating-users-in-kubernetes-6a5a2cfd8c71
 -----
 apt-get update
 apt-get install -y docker.io
